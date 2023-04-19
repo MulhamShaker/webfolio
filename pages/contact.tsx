5e9c8de901e56a7858e2fad34a26b0c8f35247e8
molham-shaker <molham.sh95@gmail.com>
2023-04-19 03:51:37 +0200

    prerfix

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -8,20 +8,20 @@ const myInfo = () => {
                 <title>My Portfolio | Contact</title>
             </Head>
 
-            <form className="container md:w-[500px] bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 w-[500px] m-15 mt-5 absolute -translate-y-1/2 -translate-x-1/2 top-2/4 left-1/2">
+            <form className="container sm:w-[400px] md:w-[500px] md:h-[400px] sm:h-[400px] sm:mt-20 md:mt-0 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700  m-15 mt-5 absolute -translate-y-1/2 -translate-x-1/2 top-2/4 left-1/2">
                 <div className="m-6">
                     <h3 className="mb-6 sm:text-sm md:text-md">Fill The Form And I Will Response As Fast As I Could</h3>
-                    <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                    <input type="email" id="email" className=" md:w-96 sm:w-80 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required></input>
+                    <label htmlFor="email" className=" block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                    <input type="email" id="email" className="sm:w-64 md:w-96 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required></input>
                 </div>
                 <div className="mb-6">
 
                     <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white m-6 w-96">Your message</label>
-                    <textarea id="message" className="  md:w-96 sm:w-80 block p-2.5 m-6  text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Leave a comment..."></textarea>
+                    <textarea id="message" className=" sm:w-64 md:w-96 block p-2.5 m-6 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Leave a comment..."></textarea>
                 </div>
                 <div className="flex items-start mb-6">
                     <div className="flex items-center h-5">
-                    <button type="submit" className="m-6 w-96 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Send</button>
+                    <button type="submit" className="m-6 sm:w-48 md:w-84 mt-20 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Send</button>
                     </div>
                 </div>
                 
